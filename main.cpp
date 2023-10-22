f83f030c08d1a6fcf1b37e58a4c9ce6c070b98dc
Kolja Kühn <kolja@heck-kuehn.de>
2023-10-22 13:31:25 +0200

    Refactor main.

--- main.cpp
+++ main.cpp
@@ -12,37 +12,8 @@ MoveGenerator move_gen;
 int most_captures = 0;
 
 constexpr uint32_t num_bits = 27;
-
-std::vector<Move> pv(128);
-int pv_index = 0;
-
-uint64_t perft(Board& board, int depth) {
-    if (depth == 0) {
-        int captures = board.number_of_captures();
-        if (captures > most_captures) {
-            most_captures = captures;
-            for (int i = 0; i < pv_index; i++) {
-                pv[i].print();
-                std::cout << " ";
-            }
-            std::cout << std::endl;
-        }
-        most_captures = std::max(most_captures, board.number_of_captures());
-        return 1;
-    }
-    uint64_t result = 0;
-    std::vector<Move> moves;
-    move_gen.generate_moves(moves, board);
-
-    for (Move move : moves) {
-        board.make_move(move);
-        pv[pv_index++] = move;
-        result += perft(board, depth - 1);
-        board.unmake_move(move);
-        pv[--pv_index] = Move();
-    }
-    return result;
-}
+constexpr uint32_t game_length = 200;
+constexpr uint32_t num_threads = 16;
 
 void print_info(int depth, int eval, uint64_t nodes, uint64_t micros) {
     auto millis = micros / 1000;
@@ -65,19 +36,11 @@ void search_test(int depth_limit) {
     }
 }
 
-void perft_test(Board& board, int depth_limit) {
-    board.print();
-    most_captures = 0;
-    for (int depth = 0; depth <= depth_limit; depth++) {
-        std::cout << perft(board, depth) << " " << most_captures << std::endl;
-    }
-}
-
 void game(int depth_limit) {
     Board board(Position::Full);
     board.print();
     TranspositionTable tt(num_bits);
-    for (int i = 0; i < 300; i++) {
+    for (int i = 0; i < game_length; i++) {
         Search search(board, tt);
         for (int depth = 1; depth <= depth_limit; depth++) {
             Timer timer;
@@ -108,7 +71,7 @@ void game(int dwarf_depth, int troll_depth, uint64_t dwarf_time_micros, uint64_t
         }
     };
 
-    for (int i = 0; i < 300; i++) {
+    for (int i = 0; i < game_length; i++) {
         Search search(board, tt);
         Timer timer;
         int depth = 1;
@@ -128,35 +91,21 @@ void game(int dwarf_depth, int troll_depth, uint64_t dwarf_time_micros, uint64_t
     }
 }
 
-int main() {
-    setup_valid_squares();
-    //Tablebase_test test;
-    //test.test_indexing(4);
-
+void parallel_perft() {
     Board board(Position::Full);
 
     std::vector<Move> moves;
     move_gen.generate_moves(moves, board);
 
-    //search_test(8);
-
-    game(8, 4, 20000000, 0);
-
-    //Perft_TT tt;
-    //Perft perft(tt);
-
-    //perft.root_perft(board, 7, true);
-    /*for (Move move : moves) {
+    Perft_TT tt;
+    for (Move move : moves) {
         board.make_move(move);
         board.print();
-
-        int num_threads = 16;
         std::vector<Perft> perfts;
         perfts.reserve(num_threads);
         for (int i = 0; i < num_threads; i++) {
             perfts.emplace_back(tt);
         }
-
 
         std::vector<std::thread> search_threads;
         for (size_t i = 0; i < num_threads; i++) {
@@ -168,13 +117,16 @@ int main() {
         }
         tt.store();
         board.unmake_move(move);
-    }*/
+    }
+}
 
+int main() {
+    setup_valid_squares();
+    //Tablebase_test test;
+    //test.test_indexing(4);
+    //search_test(8);
+    // parallel_perft();
 
-    //Board board(Position::Endgame);
-    //perft_test(board, 6);
-
-    //perft_test(board_2, 5);
-
+    game(8, 4, 20000000, 0);
     return 0;
 }
