45d53a0a664058617abd4e524c54bcdec69b4c2b
Kolja Kühn <kolja@heck-kuehn.de>
2023-11-10 19:06:55 +0100

    Remove unused functionality from search.

--- Search.h
+++ Search.h
@@ -16,21 +16,17 @@ private:
     TranspositionTable& tt;
     const EvalParameters eval_params;
 
+    EvalType null_window_search(uint8_t depth, EvalType beta);
+
+    bool tt_probe(Move &move, EvalType &alpha, EvalType &beta, int depth);
+
 public:
     static int new_depth(int depth, Move move);
-
-    EvalType nega_minimax(uint8_t depth);
 
     Search(Board board, TranspositionTable& tt, EvalParameters eval_params) : board(std::move(board)), tt(tt),
                                                                               eval_params(eval_params) {};
 
-    EvalType nega_max(uint8_t depth, EvalType alpha, EvalType beta);
-
-    EvalType null_window_search(uint8_t depth, EvalType beta);
-
     EvalType pv_search(uint8_t depth, EvalType alpha, EvalType beta);
-
-    bool tt_probe(Move &move, EvalType &alpha, EvalType &beta, int depth);
 
     void reset_nodes();
 
